3d485b1ee0c2f2d02e46893ff05730ba184af874
hpols <atschpe@gmail.com>
2020-08-14 08:15:20 +0200

    Work so far
    restarting a fresh repo.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,37 @@
+/* Global Variables */
+const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
+const apiHolder = '?key=';
+const queryHolder = '&of=json&txt='
+const langHolder= '&lang=en';
+
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
+    // get input from form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
-
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+	//send input to server for handling and retrieve result
+	postText('/meaning', {
+		input: formText
+	})
+	.then(function (res) {
+		document.getElementById('results').innerHTML = res.message
+	})
 }
 
+const postText = async (url =' ', data = {} ) => {
+	const response = await fetch(url, {
+		method: 'POST',
+		credentials: 'same-origin',
+		headers : {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	});
+	try { 
+		const inputData = await response.json();
+		return inputData;
+	} catch (error) {
+		console.log('error', error);
+	}
+}
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,43 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const meaningCloud = require('meaning-cloud')
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+/*adding*/
+const bodyPaser = require('body-parser')
+const cors = require('cors')
+
 const app = express()
+/*adding*/
+app.use(cors())
+app.use(bodyPaser.json()) //for json
+app.use(bodyPaser.urlencoded()) { // for url-encoded values
+	extended: true
+}
 
 app.use(express.static('dist'))
+
+const baseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
+const apiHolder = '?key=';
+const queryHolder = '&of=json&txt='
+const langHolder= '&lang=en';
+
+app.post('/meaning', function(req, res){
+	const request = await fetch (baseUrl + apiHolder + process.env.API_KEY + queryHolder + req.body.input + langHolder + projectData.lang)
+	try {
+			const receivedData = await request.json();
+			console.log(receivedData);
+			res.send(receivedData);
+		} catch (error) {
+			console.log('error', error);
+			//TODO: send something back to flag the error
+		}
+})
+
+
 
 console.log(__dirname)
 
